bc108d26a6e5880a47f22b2f23cdaed2d4e31361
mariopino <info@quequiereshacer.es>
2019-10-26 00:06:48 +0200

    Fix

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ app.get('/intentions', async function (req, res) {
   con.query('SELECT json FROM validator_intention WHERE 1 ORDER BY id DESC LIMIT 1;', function(err, rows, fields) {
     if (err) throw err;  
 
-    console.log(`json:`, rows[0].json)
+    console.log(`json:`, rows[0]['json'])
     
     //var data = JSON.parse(stripslashes(rows[0]))
     
