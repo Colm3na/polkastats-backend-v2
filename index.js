65a69b51243a8d7ff711c519cdda9570c3401235
mariopino <info@quequiereshacer.es>
2019-10-26 18:54:07 +0200

    Cleanup

--- index.js
+++ index.js
@@ -10,8 +10,30 @@ var app = express();
 
 var mysql = require('mysql');
 
+// Backend port
+var backendPort = 8443;
+
 // Local Polkadot node
 var wsProviderUrl = 'ws://127.0.0.1:9944';
+
+// MySQL database connection
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "polkastats",
+  password: "polkastats",
+  database: "polkastats",
+});
+
+// SSL certificate files
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/polkastats.io/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/polkastats.io/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/polkastats.io/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 // CORS
 app.use((req, res, next) => {
@@ -23,14 +45,6 @@ app.use((req, res, next) => {
 });
 
 app.get('/system', async function (req, res) {
-  
-  // Connect to MySQL
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "polkastats",
-    password: "polkastats",
-    database: "polkastats",
-  });
 
   // Get last state
   con.query('SELECT chain, client_name, client_version, timestamp FROM system WHERE 1 ORDER BY id DESC LIMIT 1;', function(err, rows, fields) {
@@ -41,14 +55,6 @@ app.get('/system', async function (req, res) {
 });
 
 app.get('/chain', async function (req, res) {
-  
-  // Connect to MySQL
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "polkastats",
-    password: "polkastats",
-    database: "polkastats",
-  });
 
   // Get last state
   con.query('SELECT block_height, session_json, timestamp FROM chain WHERE 1 ORDER BY id DESC LIMIT 1;', function(err, rows, fields) {
@@ -64,74 +70,7 @@ app.get('/chain', async function (req, res) {
 });
 
 
-app.get('/session', async function (req, res) {
-  
-  //
-  // Initialise the provider to connect to the local polkadot node
-  //
-  const provider = new WsProvider(wsProviderUrl);
-
-  //
-  // Create the API and wait until ready
-  //
-  const api = await ApiPromise.create(provider);
-
-  //
-  // Get session info
-  //
-  const session = await api.derive.session.info();
-
-  //
-  // Disconnect. TODO: Reuse websocket connection
-  //
-  provider.disconnect();
-
-  //
-  // Outputs JSON
-  //
-  res.json(session);
-
-});
-
-app.get('/bestblocknumber', async function (req, res) {
-  
-  //
-  // Initialise the provider to connect to the local polkadot node
-  //
-  const provider = new WsProvider(wsProviderUrl);
-
-  //
-  // Create the API and wait until ready
-  //
-  const api = await ApiPromise.create(provider);
-
-  //
-  // Get best block number
-  //
-  const blockNumber = await api.derive.chain.bestNumber();
-
-  //
-  // Disconnect. TODO: Reuse websocket connection
-  //
-  provider.disconnect();
-
-  //
-  // Outputs JSON
-  //
-  res.json(blockNumber);
-
-});
-
-
 app.get('/validators', async function (req, res) {
-
-  // Connect to MySQL
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "polkastats",
-    password: "polkastats",
-    database: "polkastats",
-  });
 
   // Get last state
   con.query('SELECT json FROM validator WHERE 1 ORDER BY id DESC LIMIT 1;', function(err, rows, fields) {
@@ -143,14 +82,6 @@ app.get('/validators', async function (req, res) {
 
 
 app.get('/intentions', async function (req, res) {
-
-  // Connect to MySQL
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "polkastats",
-    password: "polkastats",
-    database: "polkastats",
-  });
 
   // Get last state
   con.query('SELECT json FROM validator_intention WHERE 1 ORDER BY id DESC LIMIT 1;', function(err, rows, fields) {
@@ -194,14 +125,6 @@ app.get('/validator/:accountId', async function (req, res) {
 
 app.get('/validator/graph/daily/:accountId', function (req, res, next) {
 
-  // Connect to MySQL
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "polkastats",
-    password: "polkastats",
-    database: "polkastats",
-  });
-
   // Last 24 hours
   con.query('SELECT id, accountId, timestamp, amount FROM validator_bonded WHERE accountId = \'' + req.params.accountId + '\' ORDER BY id DESC LIMIT 288;', function(err, rows, fields) {
     if (err) throw err;
@@ -212,14 +135,6 @@ app.get('/validator/graph/daily/:accountId', function (req, res, next) {
 
 app.get('/validator/graph/weekly/:accountId', function (req, res, next) {
 
-  // Connect to MySQL
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "polkastats",
-    password: "polkastats",
-    database: "polkastats",
-  });
-
   // Last 7 days
   con.query('SELECT id, accountId, timestamp, amount FROM validator_bonded WHERE accountId = \'' + req.params.accountId + '\' AND DATE_FORMAT(FROM_UNIXTIME(`timestamp`), "%d/%m/%Y %H:%i:%s") LIKE "%00:00:%" ORDER BY id DESC LIMIT 7;', function(err, rows, fields) {
     if (err) throw err;  
@@ -229,14 +144,6 @@ app.get('/validator/graph/weekly/:accountId', function (req, res, next) {
 });
 
 app.get('/validator/graph/monthly/:accountId', function (req, res, next) {
-
-  // Connect to MySQL
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "polkastats",
-    password: "polkastats",
-    database: "polkastats",
-  });
 
   // Last month (30 days)
   con.query('SELECT id, accountId, timestamp, amount FROM validator_bonded WHERE accountId = \'' + req.params.accountId + '\' AND DATE_FORMAT(FROM_UNIXTIME(`timestamp`), "%d/%m/%Y %H:%i:%s") LIKE "%00:00:%" ORDER BY id DESC LIMIT 720;', function(err, rows, fields) {
@@ -250,14 +157,6 @@ app.get('/validator/graph/monthly/:accountId', function (req, res, next) {
 
 app.get('/intention/graph/daily/:accountId', function (req, res, next) {
 
-  // Connect to MySQL
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "polkastats",
-    password: "polkastats",
-    database: "polkastats",
-  });
-
   // Last 24 hours
   con.query('SELECT id, accountId, timestamp, amount FROM validator_bonded WHERE accountId = \'' + req.params.accountId + '\' ORDER BY id DESC LIMIT 288;', function(err, rows, fields) {
     if (err) throw err;
@@ -267,14 +166,6 @@ app.get('/intention/graph/daily/:accountId', function (req, res, next) {
 });
 
 app.get('/intention/graph/weekly/:accountId', function (req, res, next) {
-
-  // Connect to MySQL
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "polkastats",
-    password: "polkastats",
-    database: "polkastats",
-  });
 
   // Last 7 days
   con.query('SELECT id, accountId, timestamp, amount FROM validator_bonded WHERE accountId = \'' + req.params.accountId + '\' AND DATE_FORMAT(FROM_UNIXTIME(`timestamp`), "%d/%m/%Y %H:%i:%s") LIKE "%00:00:%" ORDER BY id DESC LIMIT 7;', function(err, rows, fields) {
@@ -286,14 +177,6 @@ app.get('/intention/graph/weekly/:accountId', function (req, res, next) {
 
 app.get('/intention/graph/monthly/:accountId', function (req, res, next) {
 
-  // Connect to MySQL
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "polkastats",
-    password: "polkastats",
-    database: "polkastats",
-  });
-
   // Last month (30 days)
   con.query('SELECT id, accountId, timestamp, amount FROM validator_bonded WHERE accountId = \'' + req.params.accountId + '\' AND DATE_FORMAT(FROM_UNIXTIME(`timestamp`), "%d/%m/%Y %H:%i:%s") LIKE "%00:00:%" ORDER BY id DESC LIMIT 720;', function(err, rows, fields) {
     if (err) throw err;  
@@ -303,49 +186,8 @@ app.get('/intention/graph/monthly/:accountId', function (req, res, next) {
 });
 
 
-/* app.get('/offline', async function (req, res) {
-  
-  //
-  // Initialise the provider to connect to the local polkadot node
-  //
-  const provider = new WsProvider(wsProviderUrl);
-
-  //
-  // Create the API and wait until ready
-  //
-  const api = await ApiPromise.create(provider);
-
-  //
-  // Get validator outages
-  //
-  const offlineEvents = await api.query.staking.recentlyOffline();
-
-  //
-  // Disconnect. TODO: Reuse websocket connection
-  //
-  provider.disconnect();
-
-  //
-  // Outputs JSON
-  //
-  res.json(offlineEvents);
-
-}); */
-
-// SSL certificate files
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/polkastats.io/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/polkastats.io/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/polkastats.io/chain.pem', 'utf8');
-
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
-
-// Starting https server
+// Start https server
 const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(8443, () => {
-	console.log('HTTPS Server running on port 8443');
+httpsServer.listen(backendPort, () => {
+	console.log(`PolkaStats v2 Backend HTTPS RPC running on port ${backendPort}`);
 });
