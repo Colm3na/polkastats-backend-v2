d1f1f2e9fc94b3146fd5060d6a8008ffdac69c71
mariopino <info@quequiereshacer.es>
2019-10-26 02:49:12 +0200

    Fix intention bonded

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ app.get('/validator/graph/monthly/:accountId', function (req, res, next) {
 
 });
 
-app.get('/offline', async function (req, res) {
+/* app.get('/offline', async function (req, res) {
   
   //
   // Initialise the provider to connect to the local polkadot node
@@ -266,7 +266,7 @@ app.get('/offline', async function (req, res) {
   //
   res.json(offlineEvents);
 
-});
+}); */
 
 // SSL certificate files
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/polkastats.io/privkey.pem', 'utf8');
@@ -285,33 +285,3 @@ const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(8443, () => {
 	console.log('HTTPS Server running on port 8443');
 });
-
-
-function stripslashes (str) {
-  // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +   improved by: Ates Goral (http://magnetiq.com)
-  // +      fixed by: Mick@el
-  // +   improved by: marrtins
-  // +   bugfixed by: Onno Marsman
-  // +   improved by: rezna
-  // +   input by: Rick Waldron
-  // +   reimplemented by: Brett Zamir (http://brett-zamir.me)
-  // +   input by: Brant Messenger (http://www.brantmessenger.com/)
-  // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
-  // *     example 1: stripslashes('Kevin\'s code');
-  // *     returns 1: "Kevin's code"
-  // *     example 2: stripslashes('Kevin\\\'s code');
-  // *     returns 2: "Kevin\'s code"
-  return (str + '').replace(/\\(.?)/g, function (s, n1) {
-    switch (n1) {
-    case '\\':
-      return '\\';
-    case '0':
-      return '\u0000';
-    case '':
-      return '';
-    default:
-      return n1;
-    }
-  });
-}
