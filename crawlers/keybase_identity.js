ef7293f63bb105990cf75c9ddcaf9760132b9ec8
mariopino <info@quequiereshacer.es>
2019-10-27 14:44:10 +0100

    Await

--- crawlers/keybase_identity.js
+++ crawlers/keybase_identity.js
@@ -16,14 +16,14 @@ const { mysqlConnParams } = require('../backend.config');
 //
 const srcPath = "/usr/local/polkastats-v2/identities/";
 const keybaseIdentityFolders = fs.readdirSync(srcPath).filter(file => fs.statSync(join(srcPath, file)).isDirectory());
-console.log(`keybase Identity Folders:`, keybaseIdentityFolders);
+//console.log(`keybase Identity Folders:`, keybaseIdentityFolders);
 const keybaseIdentities = keybaseIdentityFolders.map(folder => {
   return {
     stashId: folder,
     username: fs.readFileSync(join(srcPath, folder, `keybase_username`), 'utf-8').replace(/(\r\n|\n|\r)/gm, "")
   }
 });
-console.log(`keybase Identities`, keybaseIdentities);
+//console.log(`keybase Identities`, keybaseIdentities);
 
 async function main () {
 
@@ -40,7 +40,7 @@ async function main () {
       //
       // Fetch identity object from Keybase
       //
-      axios.get(`https://keybase.io/_/api/1.0/user/lookup.json?username=${identity.username}`)
+      await axios.get(`https://keybase.io/_/api/1.0/user/lookup.json?username=${identity.username}`)
         .then(function (response) {
           // handle success
           console.log(`Keybase Identity:`, response);
