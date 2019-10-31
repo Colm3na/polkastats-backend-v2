6b7d2c4d5650586e14d79d2ece1817b791a9d19f
mariopino <info@quequiereshacer.es>
2019-10-31 12:08:09 +0100

    Debug

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.get('/intentions', function (req, res) {
   var validators = null
   con.query('SELECT json FROM validator_intention WHERE 1 ORDER BY id DESC LIMIT 1; SELECT json FROM validator WHERE 1 ORDER BY id DESC LIMIT 1;', [2, 1], function(err, rows, fields) {
     if (err) throw err;
-
+    console.log(`rows`, rows)
     
     console.log(`intentions`, rows[0]['json'])
     console.log(`validators`, rows[1]['json'])
