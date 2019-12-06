a75ccda2b44ee139ec874888c98c6f49e9285fa0
mariopino <info@quequiereshacer.es>
2019-12-06 18:37:11 +0100

    Testing phragment

---
+++ crawlers/phragment.js
@@ -0,0 +1,53 @@
+// @ts-check
+// Required imports
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+
+// Promise MySQL lib
+const mysql = require('mysql2/promise');
+
+// Import config params
+const {
+  wsProviderUrl,
+  mysqlConnParams
+} = require('../backend.config');
+
+async function main () {
+
+  //
+  // Database connection
+  //
+  const conn = await mysql.createConnection(mysqlConnParams);
+  
+  //
+  // Initialise the provider to connect to the local polkadot node
+  //
+  const provider = new WsProvider(wsProviderUrl);
+
+  // Create the API and wait until ready
+  const api = await ApiPromise.create(provider);
+
+  //
+  // Get block height, total issuance and session info
+  //
+
+  const [validatorCount, minimumValidatorCount] = await Promise.all([
+    api.chain.staking.validatorCount(),
+    api.chain.staking.minimumValidatorCount()
+  ]);
+
+  console.log(validatorCount);
+  console.log(minimumValidatorCount);
+
+//   if (blockHeight && session && totalIssuance) {
+//     console.log(`block_height: ${blockHeight} block_height_finalized: ${blockHeightFinalized} session: ${JSON.stringify(session)} total_issuance: ${totalIssuance}`);
+//     var sqlInsert = 'INSERT INTO chain (block_height, block_height_finalized, session_json, total_issuance, timestamp) VALUES (\'' + blockHeight + '\', \'' + blockHeightFinalized + '\', \'' + JSON.stringify(session) + '\', \'' + totalIssuance + '\', UNIX_TIMESTAMP());';
+//     let [rows, fields] = await conn.execute(sqlInsert, [2, 2]);
+//   }
+
+  //
+  // Disconnect. TODO: Reuse websocket connection
+  //
+  provider.disconnect();
+}
+
+main().catch(console.error).finally(() => process.exit());
