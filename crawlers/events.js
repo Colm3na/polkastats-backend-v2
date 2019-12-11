b059abd86b4918055a79e3535a7fadadb5523287
mariopino <info@quequiereshacer.es>
2019-12-11 15:15:08 +0100

    Debug

--- crawlers/events.js
+++ crawlers/events.js
@@ -14,7 +14,7 @@ async function main () {
     // Block height is not 100% accurate, get duplicated in some cases
     const blockHeight = await api.derive.chain.bestNumber();
 
-    console.log(`\nReceived ${events.length} events at block #${blockHeight}:`);
+    console.log(`\nReceived ${events.length} events at block #${blockHeight}:\n`);
 
     // Loop through the Vec<EventRecord>
     events.forEach((record, index) => {
@@ -23,7 +23,7 @@ async function main () {
       const types = event.typeDef;
 
       // Show what we are busy with
-      console.log(`\t${index} ${event.section}:${event.method}:: (phase=${phase.toString()})`);
+      console.log(`\t${index+1}. ${event.section}:${event.method}:: (phase=${phase.toString()})`);
       console.log(`\t\t${event.meta.documentation.toString()}`);
 
       // Loop through each of the parameters, displaying the type and data
