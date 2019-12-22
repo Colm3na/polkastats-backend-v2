cc21533dc3ca1a041ce3697e7fdd60d7e7d8fcea
mariopino <info@quequiereshacer.es>
2019-12-22 12:05:57 +0100

    Fix account crawler

--- crawlers/account.js
+++ crawlers/account.js
@@ -19,21 +19,21 @@ async function main () {
   const api = await ApiPromise.create();
   
   // Fetch active accounts
-  
   const accounts = await api.derive.accounts.indexes();
 
+  // Log active accounts
   console.log(JSON.stringify(accounts, null, 2));
 
-  const accountsArray = JSON.parse(JSON.stringify(accounts))
-
-  accountsArray.forEach( async account => {
-    console.log(JSON.stringify(account));
-    var sqlInsert = 'INSERT INTO account_index (accountId, accountIndex) VALUES (\'' + account.accountId + '\', \'' + account.accountIndex + '\');';
-    let [rows, fields] = await conn.execute(sqlInsert, [2, 2]);     
-  });
+  // Insert in DB
+  for (var key in accounts ) {
+    if (accounts.hasOwnProperty(key)) {
+      console.log(key + " -> " + accounts[key]);
+      var sqlInsert = 'INSERT INTO account_index (accountId, accountIndex) VALUES (\'' + key + '\', \'' + accounts[key] + '\');';
+      let [rows, fields] = await conn.execute(sqlInsert, [2, 2]);
+    }
+  }
 
   conn.end();
-
 }
 
 main().catch(console.error).finally(() => process.exit());
